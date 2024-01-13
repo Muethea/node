206e62a76e1556ed07a2b4f9546eb716a505957e
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2024-01-13 10:44:42 +0200

    object aside

--- express/index.js
+++ express/index.js
@@ -1,13 +1,21 @@
-const express = require('express')
+// const express = require('express')
 
-const app = express()
+// const app = express()
 
-const port = 3000
+// const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Ola Mundo')
-})
+// app.get('/', (req, res) => {
+//   res.send('Ola Mundo')
+// })
 
-app.listen(port, () => {
-  console.log(`Rodando na porta ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Rodando na porta ${port}`)
+// })
+
+var obj = {
+  greet: 'hello'
+}
+
+var prop = 'gret'
+
+console.log(obj[prop])
