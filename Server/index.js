4bcfee89a388c5411ebc7c6cacc418858aab4eb1
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2024-01-19 14:21:45 +0200

    feat: Json

--- Server/index.js
+++ Server/index.js
@@ -5,8 +5,12 @@ var port = 1337
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.createReadStream(__dirname + '/index.html').pipe(res)
-    
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+
+    var obj = {
+      firstname: 'Fernando',
+      lastname: 'Muethea'
+    }
+    res.end(JSON.stringify(obj))
   })
   .listen(port, console.log(`Rodando na porta ${port}`))
