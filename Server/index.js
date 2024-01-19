c3b221f2c10b472737e63054e8f2ec7f1d17ac4e
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2024-01-19 14:14:51 +0200

    feat: Stream

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,7 @@ var port = 1337
 http
   .createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' })
-    var html = fs.readFileSync(__dirname + '/index.html')
-    res.end(html)
+    fs.createReadStream(__dirname + '/index.html').pipe(res)
+    
   })
   .listen(port, console.log(`Rodando na porta ${port}`))
