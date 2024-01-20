294299eb45e7516e58bbe23fa9998ce97d90ff4c
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2024-01-20 14:52:44 +0200

    feat: express, nodemon

--- express/templete/index.js
+++ express/templete/index.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var port = process.env.port || 3001
 
 app.set('view engine', 'ejs')
@@ -10,7 +12,13 @@ app.get('/', (req, res) => {
 })
 
 app.get('/person/:id', (req, res) => {
-  res.render('index', { ID: req.params.id })
+  res.render('index', { ID: req.params.id, Qstr: req.query.qstr })
+})
+
+app.post('/person', urlencodedParser, (req, res) => {
+  res.send('Thank you')
+  console.log(req.body.firstname)
+  console.log(req.body.lastname)
 })
 
 app.listen(port, console.log(`Rodando na porta ${port}`))
