b0e68f68774bca355df51b8ad10306cb0a096a5c
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2024-01-26 18:11:40 +0200

    update node

--- structuring/index.js
+++ structuring/index.js
@@ -1,11 +1,37 @@
 const express = require('express')
-
 const app = express()
+const mysql = require('mysql')
+const ejs = require('ejs')
 
-const port = process.env.port || 3001
-
-app.get('/', (req, res) => {
-  res.send('Ola Mundo')
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'person'
 })
 
-app.listen(port, console.log(`Rodando na porta ${port}`))
+db.connect(error => {
+  if (error) {
+    console.log('Nao foi possivel conectar com o banco de dados', error)
+  } else {
+    console.log('Banco de dados conectado com sucesso')
+  }
+})
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  db.query('SELECT * FROM person', (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err)
+      res.status(500).send('Internal Server Error')
+    } else {
+      res.render('index', { data: results })
+    }
+  })
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
